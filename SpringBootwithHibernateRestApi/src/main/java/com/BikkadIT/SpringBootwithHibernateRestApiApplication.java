fc3e55846f924c8be56aa00b138959d59d5154da
unknown <udaysrathod95@gmail.com>
2022-08-11 14:47:00 +0530

    project added to central repository

---
+++ SpringBootwithHibernateRestApi/src/main/java/com/BikkadIT/SpringBootwithHibernateRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootwithHibernateRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootwithHibernateRestApiApplication.class, args);
+	}
+
+}
