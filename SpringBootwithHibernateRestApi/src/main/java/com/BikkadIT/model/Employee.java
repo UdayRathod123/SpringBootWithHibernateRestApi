293b52c77e91edb68d72a747ee62678a55d0a176
unknown <udaysrathod95@gmail.com>
2022-08-12 18:02:17 +0530

    SPRING-2 :CreateEntityClass

--- SpringBootwithHibernateRestApi/src/main/java/com/BikkadIT/model/Employee.java
+++ SpringBootwithHibernateRestApi/src/main/java/com/BikkadIT/model/Employee.java
@@ -1,5 +1,43 @@
 package com.BikkadIT.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
 
+	@Id
+	private int uid;
+	private String name;
+	private String uname;
+	private String upwd;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getUpwd() {
+		return upwd;
+	}
+	public void setUpwd(String upwd) {
+		this.upwd = upwd;
+	}
+	@Override
+	public String toString() {
+		return "Employee [uid=" + uid + ", name=" + name + ", uname=" + uname + ", upwd=" + upwd + "]";
+	}
+	
 }
